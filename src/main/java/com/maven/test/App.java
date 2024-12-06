4812b581d4e748a1e367c4f31b90962ae47a2b84
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-06 17:51:34 -0500

    App

--- src/main/java/com/maven/test/App.java
+++ src/main/java/com/maven/test/App.java
@@ -1,5 +1,8 @@
 package com.maven.test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
